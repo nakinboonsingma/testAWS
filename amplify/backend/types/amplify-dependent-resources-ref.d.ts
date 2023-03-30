184276eef6d753bd77b813fb4ee0fdea9e372b6f
nakhinDev <nakhin_boo@nationgroup.com>
2023-03-30 13:53:53 +0700

    Amplify SDK Installation

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "testAmplify": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
